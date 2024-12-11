a5fea5753d8ec99d06b750c57cb154bb8009c3f1
juan pablo <jau00@iesemilidarder.com>
2024-12-11 16:14:55 +0100

    bugs

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -15,8 +15,10 @@ export function Inicio() {
                     <div className="aboutme">
                         <h1>Juan Pablo Aristizábal Uchima</h1>
                         <h2>Desarrollador Web</h2>
-                        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi illum facere sint modi, enim accusamus nemo dolorem ducimus error sunt non? Corrupti libero aut praesentium!
-                        </p>
+                        <p> Desarrollador Web Especializado en el desarrollo de aplicaciones web dinámicas.
+                        Comprometido con la mejora continua, la atención al detalle y la entrega de proyectos de alta
+                        calidad. Busco aportar mis habilidades técnicas y creatividad para resolver problemas y superar
+                        desafíos en entornos colaborativos.</p>
 
                         <div className="botonKM">
                             <Link to="/Contact" className="botonInicio" type="button">Conóceme</Link>
@@ -24,10 +26,10 @@ export function Inicio() {
 
                         <div className="iconos">
                             <ul className="redesSociales">
-                                <li ><a target="_blank" href="https://es-es.facebook.com/"><img src="./iconos/facebook-brands-solid.svg" alt="" className="icon" /></a></li>
-                                <li><a target="_blank" href="https://github.com/jp99-9"><img src="./iconos/github-brands-solid.svg" alt="" className="icon" /></a></li>
-                                <li><a target="_blank" href="https://www.instagram.com/"><img src="./iconos/instagram-brands-solid.svg" alt="" className="icon" /></a></li>
-                                <li><a target="_blank" href="https://www.linkedin.com/"><img src="./iconos/linkedin-in-brands-solid.svg" alt="" className="icon" /></a></li>
+                                <li ><a target="_blank" rel="noreferrer" href="https://es-es.facebook.com/"><img src="./iconos/facebook-brands-solid.svg" alt="" className="icon" /></a></li>
+                                <li><a target="_blank" rel="noreferrer" href="https://github.com/jp99-9"><img src="./iconos/github-brands-solid.svg" alt="" className="icon" /></a></li>
+                                <li><a target="_blank" rel="noreferrer" href="https://www.instagram.com/"><img src="./iconos/instagram-brands-solid.svg" alt="" className="icon" /></a></li>
+                                <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/"><img src="./iconos/linkedin-in-brands-solid.svg" alt="" className="icon" /></a></li>
                             </ul>
                         </div>
 
