7bbe6f36d8281453914ee80225b3aabf555e4bbf
juan pablo <jau00@iesemilidarder.com>
2024-12-11 16:18:06 +0100

    bugs

--- src/componentes/Proyect.jsx
+++ src/componentes/Proyect.jsx
@@ -3,7 +3,7 @@ export function Proyect({ id, nombre, descripcion, tecnologia, imagen }) {
     return (
         <div className="proyecto">
             <h2>{nombre}</h2>
-            <a target="_blank" href="https://github.com/jp99-9"> <img src={imagen} alt={nombre} /></a>
+            <a target="_blank" rel="noreferrer" href="https://github.com/jp99-9"> <img src={imagen} alt={nombre} /></a>
             <p className="descripcion">{descripcion}</p>
             <p className="tecnologia">{tecnologia}</p>
         </div>
